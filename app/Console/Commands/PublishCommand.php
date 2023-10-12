e02ec717a7a78b63066f7adc1773d830a0ecf18c
kozyrev42 <alexander.kozyrev@pluscards.ru>
2023-10-12 15:52:42 +0700

    вынес данные подключения в конфиг; отправка массива в сообщении

--- app/Console/Commands/PublishCommand.php
+++ app/Console/Commands/PublishCommand.php
@@ -41,19 +41,32 @@ class PublishCommand extends Command
     public function handle()
     {
         // создаём новое соединение с RabbitMQ-сервером
-        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $connection = new AMQPStreamConnection(
+            config('rabbitmq.host'),
+            config('rabbitmq.port'),
+            config('rabbitmq.user'),
+            config('rabbitmq.password')
+        );
         // создание канала на этом соединении
         $channel = $connection->channel();
 
+
+        // следующие 3 метода обычно делаются в админ панели, а не в коде
         // Объявление обменника
-        $channel->exchange_declare('laravel_exch', 'direct', false, true, false);
+        //$channel->exchange_declare('laravel_exch', 'direct', false, true, false);
         // Объявление очереди с именем 'laravel_que', Если такой очереди нет, она будет создана
-        $channel->queue_declare('laravel_que', false, true, false, false);
+        //$channel->queue_declare('laravel_que', false, true, false, false);
         // Привязка объявленной выше очереди 'laravel_que' к обменнику 'laravel_exch'
-        $channel->queue_bind('laravel_que', 'laravel_exch');
+        //$channel->queue_bind('laravel_que', 'laravel_exch');
 
+        $data = [
+            'title'=>'заголовок',
+            'content'=>'контент'
+        ];
 
-        $msg = new AMQPMessage('моё-тестовое-сообщение');
+        $data_json = json_encode($data);
+
+        $msg = new AMQPMessage($data_json);
         // Отправка сообщения на обменник 'laravel_exch'
         $channel->basic_publish($msg, 'laravel_exch');
 
